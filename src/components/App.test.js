12b1f6c17dbb697fef63b9712f77e4bf6fad3054
Samuel Mast <samuel.a.mast@gmail.com>
2018-05-04 09:48:19 -0600

    First commit

---
+++ src/components/App.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+const app = shallow(<App />);
+
+it('renders correctly', () => {
+	expect(app).toMatchSnapshot();
+});
+
+it('initializes the `state` with an empy list of gifts', () => {
+	expect(app.state().gifts).toEqual([]);
+});
+
+it('adds a new gift to `state` when clicking `add gift` button', () => {
+	//find add gift button and simulate click
+	app.find('.btn-add').simulate('click');
+
+	expect(app.state().gifts).toEqual([{ id: 1 }])
+});
+
+it('adss new gift to rendered list when clicking the `add gift` button', () => {
+	app.find('.btn-add').simulate('click');
+
+	expect(app.find('.gift-list').children().length).toEqual(2);
+})
